07290ef3bd1b285d9af2676596e2fb7a589af1ba
skelmis <ethan@koldfusion.xyz>
2023-03-21 20:24:03 +1300

    fix: Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ RUN go build -v -o server
 FROM debian:buster-slim
 RUN set -x && apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
     ca-certificates && \
-    rm -rf /var/lib/apt/lists/*
+    rm -rf /var/lib/apt/lists/* \
 
 COPY --from=builder /app/server /app/server
 
--- main.go
+++ main.go
@@ -14,8 +14,14 @@ func init() {
 func main() {
 	r := gin.Default()
 	if os.Getenv("GIN_MODE") == "release" {
-		_ = r.SetTrustedProxies([]string{"172.18.0.3"})
+		r.SetTrustedProxies([]string{"172.18.0.3"})
 	}
+
+	r.GET("", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "home",
+		})
+	})
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
